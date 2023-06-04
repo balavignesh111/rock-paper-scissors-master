36b9fd9b0f0ec5dad0ac4c9306ab9b2ed74cb7fe
balavignesh111 <balavignesh1018@gmail.com>
2023-06-04 18:45:45 +0530

    first two UI part completed

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// console.log('i am working');
+
+// take input buttons
+let rulePopUpElement = document.getElementById('rule-popup');
+
+// function to open and close
+function openpopup(){
+  rulePopUpElement.style.display = 'block';
+}
+
+function closepopup(){
+  rulePopUpElement.style.display = 'none';
+}
